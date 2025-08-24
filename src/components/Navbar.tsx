ce5ad73335e279d9813c7b4b12f7ecd089bd4876
unKnown_NG <mohammeddaiyaan2005@gmail.com>
2025-08-24 12:07:06 +0530

    WIP: local navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-yellow-500/30 rounded-2xl backdrop-blur-sm sticky top-0 z-50">
+    <nav className="m-0 p-0 backdrop-blur-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px6 lg:px-8">
         <div className="flex items-center justify-between h-[72px]">
           <div className="flex items-center ">
             <div className="flex-shrink-0">
-            <Image src = "/logo.png" alt="" width={50} height={50} />
+            <Image src = "/logo/logo.png" alt="" width={70} height={70} />
             </div>
           </div>
           <div className="hidden md:block">
